bb488ac215310619c2758fb27b5c575de1e6f922
luoye <luoyefe@gmail.com>
2017-11-01 15:42:34 +0800

    update: 添加console

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ module.exports = app => {
 
     // 每小时更新一次天气
     schedule.scheduleJob('00 * * * *', function() {
+        console.log('更新天气啦', new Date());
         app.io.emit('initWeather')
     });
     // 每天12点更新下日历
     schedule.scheduleJob('00 00 * * *', function() {
         // console.log('Today is recognized by Rebecca Black!');
+        console.log('更新日历啦', new Date());
         app.io.emit('initCalendar')
     });
 };
